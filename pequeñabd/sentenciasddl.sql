737f64137666f1bd2741252edbdc18bfdcc1fe0b
Rocio <ralbarracintrab@gmail.com>
2025-06-05 20:00:01 -0300

    Agregamos sentencias para crear la base de datos 'pequeñadb', falta unión

--- pequeñabd/sentenciasddl.sql
+++ pequeñabd/sentenciasddl.sql
@@ -1,20 +1,20 @@
-CREATE TABLE "registro_de_productos" (
+CREATE TABLE "registro_productos" ( --inventory
 	"id_inventario"	INTEGER,
 	"id_producto"	INTEGER NOT NULL,
-	"id_tienda"	INTEGER NOT NULL,
+	"id_tienda"		INTEGER NOT NULL,
 	PRIMARY KEY("id_inventario" AUTOINCREMENT),
-	FOREIGN KEY("id_producto") REFERENCES productos("id_producto"),
-	FOREIGN KEY("id_tienda") REFERENCES tiendas("id_tienda")
+	FOREIGN KEY("id_producto")	REFERENCES productos("id_producto"), --Tablas a unir proximamente
+	FOREIGN KEY("id_tienda") 	REFERENCES tiendas("id_tienda")
 );
-CREATE TABLE "tiendas" (
+CREATE TABLE "tiendas" ( --locales (página vitual cuenta?)
 	"id_tienda"	INTEGER,
 	"nombre"	VARCHAR(40) NOT NULL UNIQUE,
 	"ubicación"	VARCHAR(50) NOT NULL UNIQUE,
-	PRIMARY KEY("id_tienda" AUTOINCREMENT),
+	PRIMARY KEY("id_tienda" AUTOINCREMENT)
 );
-CREATE TABLE "empleados" (
+CREATE TABLE "empleados" ( --datos de los epleado
 	"id_empleado" INTEGER,
-	"nombre"	VARCHAR(40) NOT null UNIQUE, --Dudoso, podemos rechazar a un empledo por el nombre
+	"nombre"	VARCHAR(40) NOT null UNIQUE, --Dudoso, podemos rechazar a un empledo por repetir el nombre
 	"apellido"	VARCHAR(40) NOT NULL UNIQUE,
 	"id_tienda" INTEGER NOT NULL UNIQUE,
 	"puesto_trabajo" VARCHAR(50) NOT NULL,
@@ -24,7 +24,17 @@ CREATE TABLE "empleados" (
 	FOREIGN KEY("id_tienda") REFERENCES tiendas("id_tienda")
 );
 CREATE TABLE "ventas" (
-	"id"	INTEGER,
-	"nombre"	
-	PRIMARY KEY("id_tienda" AUTOINCREMENT),
+	"id_venta"		INTEGER,
+	"fecha"			INTEGER NOT null,	
+	"mes"			INTEGER NOT null,
+	"año"			INTEGER NOT null,
+	"id_pago"		INTEGER NOT NULL,
+	"monto" 		INTEGER NOT NULL,
+	"id_empleado"	INTEGER,
+	"id_tienda" 	INTEGER,
+	"id_compra" 	INTEGER,
+	PRIMARY KEY("id_venta" AUTOINCREMENT),
+	FOREIGN KEY("id_pago") REFERENCES clientes_pago("id_pago"),
+	FOREIGN KEY("id_empleado") REFERENCES empleados("id_empleado")
+	FOREIGN KEY("id_compra") REFERENCES clientes_compras("id_compra") --Fijarse como esta escrito en el de Eve (al unir todo)
 );
