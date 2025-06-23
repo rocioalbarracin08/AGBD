f6213fa9cc85de88b1a0253cae3e79902adab795
Rocio <ralbarracintrab@gmail.com>
2025-06-23 12:08:17 -0300

    Completando la pequeña base de datos

--- pequeñabd/contenidoPequeñaDB.sql
+++ pequeñabd/contenidoPequeñaDB.sql
@@ -2,67 +2,19 @@
 
 -- Tabla: productos
 INSERT INTO productos (name, id_categoria, precio) 
-VALUES ("Broche hawaiano celeste", 4, 3000),
-("Broche hawaiano rosa", 4, 3000),
-("Broche flor violeta mate", 4, 3000),
-("Broche rectangular verde mate", 4, 3000),
-("Broche rectangular amarillo", 4, 3000),
-("Broche flor rojo mate", 4, 3000),
-("Diadema trenza de flor dorada", 5, 2700),
-("Diadema trenza de flor plateada", 5, 2700),
-("Diadema con perlas", 5, 2700),
-("Vincha en trenza tejida color negro", 5, 2900),
-("Vincha en tela básica color blanco", 5, 2900),
-("Vincha en tela básica color beige", 5, 2900),
-("Scrunchie basica negra", 6, 900),
-("Scrunchie basica lila", 6, 900),
-("Scrunchie basica rosa", 6, 900),
-("Scrunchie basica rosa", 6, 3000),
-("Scrunchie basica rosa", 6, 3000),
-("Scrunchie basica rosa", 6, 3000); -- tE FALTA UN DATO DE LA FORYNKEY
+VALUES ("Broche hawaiano celeste", 11, 3000),
+("Broche hawaiano rosa", 11, 3000),
+("Broche flor violeta mate", 11, 3000),
+("Broche rectangular verde mate", 11, 3000),
+("Broche rectangular amarillo", 11, 3000),
+("Broche flor rojo mate", 11, 3000),
+--MODIFICAMOS IDs equivocados -> UPDATE productos set id_categoria = 12 WHERE id_categoria = 5
+("Scrunchie basica lisa", 13, 600),
+("Scrunchie color liso con perlas", 13, 1000),
+("Scrunchie estampado con perlas", 13, 900),
+("Scrunchie estampado", 13, 600) -- TE FALTA UN DATO DE LA FORYNKEY
+ -- AGREGAAAR
 
-("", 7, 3000), -- AGREGAAAR
-("", 7, 3000),
-("", 7, 3000),
-("", 7, 3000),
-("", 7, 3000),
-("", 7, 3000),
-("", 8, 3000),
-("", 8, 3000),
-("", 8, 3000),
-("", 8, 3000),
-("", 8, 3000),
-("", 8, 3000),
-("", 9, 3000),
-("", 9, 3000),
-("", 9, 3000),
-("", 9, 3000),
-("", 9, 3000),
-("", 9, 3000),
-("", 10, 3000),
-("", 10, 3000),
-("", 10, 3000),
-("", 10, 3000)
-("", 10, 3000)
-("", 10, 3000)
-("", 11, 3000)
-("", 11, 3000)
-("", 11, 3000)
-("", 11, 3000)
-("", 11, 3000)
-("", 11, 3000)
-("", 12, 3000)
-("", 12, 3000)
-("", 12, 3000)
-("", 12, 3000)
-("", 12, 3000)
-("", 12, 3000)
-("", 13, 3000)
-("", 13, 3000)
-("", 13, 3000)
-("", 13, 3000)
-("", 13, 3000)
-("", 13, 3000)
 
 -- Tabla: registro_productos
 
